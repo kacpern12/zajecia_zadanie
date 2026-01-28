d6b8b2bcfcc7366002cc9bcea2c7c190b405c064
kacpern <kacpern@mat.umk.pl>
2026-01-28 14:54:19 +0100

    Dodanie sprawdzenia czy liczba jest naturalna

--- liczba.cpp
+++ liczba.cpp
@@ -4,7 +4,11 @@ using namespace std;
 int main() {
     int n;
     if (cin >> n) {
-        cout << n << endl;
+        if(n>0){
+		cout<<n<<endl;
+}else{
+	cout<<"Blad: To nie jest liczba dodatnia naturalna"<<endl;
+}
     }
     return 0;
 }
