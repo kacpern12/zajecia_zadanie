a21c50b7845d2768544a38b8a65b6d8d900f5ffc
kacpern <kacpern@mat.umk.pl>
2026-01-28 14:46:07 +0100

    Dodanie logiki z liczba doskonala

--- liczba.cpp
+++ liczba.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
+bool czy_doskonala(int n) {
+    if (n < 2) return false;
+    int suma = 1;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            suma += i;
+            if (i * i != n) suma += n / i;
+        }
+    }
+    return suma == n;
+}
+
+
 int main() {
     int n;
     if (cin >> n) {
-        cout << n << endl;
+        cout << n <<(czy_doskonala(n) ? " jest liczba doskonala " : " nie jest liczba doskonala")<< endl;
     }
     return 0;
 }
